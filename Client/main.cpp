6efa1211b8e8c9ad1e4576832b1ede7ffa948bf6
Vlad Hryshko <hryshko_vv@ffeks.dnu.edu.ua>
2024-10-31 13:47:29 +0200

    Labwork4 TCP

---
+++ Client/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <Ws2tcpip.h>
+#include <winsock2.h>
+#include <fstream>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 27015
+#define BUFFER_SIZE 1024
+
+void startClient() {
+    WSADATA wsaData;
+    SOCKET clientSocket;
+    sockaddr_in serverAddr;
+    SetConsoleOutputCP(CP_UTF8);
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Не вдалося ініціалізувати Winsock. Код помилки: " << WSAGetLastError() << std::endl;
+        return;
+    }
+
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Не вдалося створити сокет. Код помилки: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return;
+    }
+
+    serverAddr.sin_family = AF_INET;
+    InetPton(AF_INET, SERVER_IP, &serverAddr.sin_addr.s_addr);
+    serverAddr.sin_port = htons(PORT);
+
+    if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Не вдалося підключитися до сервера. Код помилки: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return;
+    }
+
+    std::cout << "Підключено до сервера." << std::endl;
+
+    char fileName[BUFFER_SIZE];
+    int fileNameSize = recv(clientSocket, fileName, BUFFER_SIZE, 0);
+    if (fileNameSize <= 0) {
+        std::cerr << "Не вдалося отримати назву файлу від сервера." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return;
+    }
+    fileName[fileNameSize] = '\0';
+
+    std::cout << "Отримано назву файлу: " << fileName << std::endl;
+
+    std::ofstream outputFile(fileName, std::ios::binary);
+    if (!outputFile.is_open()) {
+        std::cerr << "Не вдалося відкрити файл для запису." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
+    int bytesReceived;
+    while ((bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+        outputFile.write(buffer, bytesReceived);
+    }
+
+    std::cout << "Файл отримано і збережено як " << fileName << std::endl;
+
+    closesocket(clientSocket);
+    WSACleanup();
+}
+
+int main() {
+    startClient();
+    return 0;
+}
---
+++ Server/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <fstream>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 27015
+#define BUFFER_SIZE 1024
+
+void startServer() {
+    WSADATA wsaData;
+    SOCKET serverSocket, clientSocket;
+    sockaddr_in serverAddr, clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+    SetConsoleOutputCP(CP_UTF8);
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Не вдалося ініціалізувати Winsock. Код помилки: " << WSAGetLastError() << std::endl;
+        return;
+    }
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Не вдалося створити сокет. Код помилки: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return;
+    }
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Не вдалося виконати прив'язку. Код помилки: " << WSAGetLastError() << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Не вдалося почати прослуховування. Код помилки: " << WSAGetLastError() << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return;
+    }
+
+    std::cout << "Сервер запущений на порту " << PORT << std::endl;
+
+    clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientAddrSize);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Не вдалося прийняти підключення. Код помилки: " << WSAGetLastError() << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return;
+    }
+
+    std::cout << "Клієнт підключено." << std::endl;
+
+    std::string fileName = "Report.docx";
+
+    send(clientSocket, fileName.c_str(), fileName.size(), 0);
+
+    std::ifstream file(fileName, std::ios::binary);
+    if (!file.is_open()) {
+        std::cerr << "Не вдалося відкрити файл." << std::endl;
+        closesocket(clientSocket);
+        closesocket(serverSocket);
+        WSACleanup();
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
+    while (file.read(buffer, sizeof(buffer)) || file.gcount() > 0) {
+        int bytesSent = send(clientSocket, buffer, file.gcount(), 0);
+        if (bytesSent == SOCKET_ERROR) {
+            std::cerr << "Помилка під час відправки даних. Код помилки: " << WSAGetLastError() << std::endl;
+            break;
+        }
+    }
+
+    std::cout << "Файл відправлено." << std::endl;
+
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+}
+
+int main() {
+    startServer();
+    return 0;
+}
